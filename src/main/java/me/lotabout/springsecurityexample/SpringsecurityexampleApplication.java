a93cd0305b76fe2128b2cf8f76ca09b96f033994
Jinzhou Zhang <lotabout@gmail.com>
2019-02-18 07:58:01 +0800

    initial commit with spring initializer

---
+++ src/main/java/me/lotabout/springsecurityexample/SpringsecurityexampleApplication.java
@@ -0,0 +1,13 @@
+package me.lotabout.springsecurityexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringsecurityexampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringsecurityexampleApplication.class, args);
+    }
+
+}
